7976c3e20543bc194c0fae99c3dfd9e03d4bff72
Neo Melonas <neo@neomelonas.com>
2009-10-27 08:02:44 -0400

    More work on functions.php
    Signed-off-by: Neo Melonas <neo@neomelonas.com>

--- lib/php/functions.php
+++ lib/php/functions.php
@@ -4,7 +4,7 @@ $con = mysql_connect($host,$name,$pwd);
 
 $localAd	= false;
 $homeAd		= false;
-$techCount	= 1;
+
 function populateName($con, $userID)
 {
 	if (!$con)
@@ -15,14 +15,14 @@ function populateName($con, $userID)
 	{
 		mysql_select_db($db, $con);
 		
-		$nameSQL = mysql_query("select userFName, userMName, userLName from resume.res_user where userID='".$userID."'");
+		$nameSQL = mysql_query("select userFName, userMName, userLName from resume_dev.res_user where userID='".$userID."'");
 		while($row = mysql_fetch_array($nameSQL))
 		{
 			$userFName	= $row['userFName'];
 			$userMName	= $row['userMName'];
 			$userLName	= $row['userLName'];
 			
-			$prefSQL = mysql_query("select middleISnick from resume.res_user_pref where userID='".$userID."'");
+			$prefSQL = mysql_query("select middleISnick from resume_dev.res_user_pref where userID='".$userID."'");
 			while($row = mysql_fetch_array($prefSQL))
 			{
 				$mISn 	= $row['middleISnick'];
@@ -44,7 +44,7 @@ function populateName($con, $userID)
 
 function populateHeader($userID)
 {
-	$sql1 = mysql_query("select userFName, userMName, userLName, userEmail, locLID, locHID from resume.res_user where userID='".$userID."'");
+	$sql1 = mysql_query("select userFName, userMName, userLName, userEmail, locLID, locHID from resume_dev.res_user where userID='".$userID."'");
 
 	while($row = mysql_fetch_array($sql1))
 	{
@@ -55,7 +55,7 @@ function populateHeader($userID)
 		$localID 	= $row['locLID'];
 		$homeID		= $row['locHID'];
 		
-		$prefSQL = mysql_query("select middleISnick, preferredPH from resume.res_user_pref where userID='".$userID."'");
+		$prefSQL = mysql_query("select middleISnick, preferredPH from resume_dev.res_user_pref where userID='".$userID."'");
 		while($row = mysql_fetch_array($prefSQL))
 		{
 			$mISn 		= $row['middleISnick'];
@@ -76,7 +76,7 @@ function populateHeader($userID)
 			if ($localID != NULL)
 			{
 				$localAd = true;
-				$localSQL = mysql_query("select locStreet1, locStreet2, locCity, locState, locZIP from resume.res_loc where locID = '".$localID."'");
+				$localSQL = mysql_query("select locStreet1, locStreet2, locCity, locState, locZIP from resume_dev.res_loc where locID = '".$localID."'");
 				while($row = mysql_fetch_array($localSQL))
 				{
 					$localS1	= $row['locStreet1'];
@@ -103,7 +103,7 @@ function populateHeader($userID)
 			if ($homeID != NULL)
 			{
 				$homeAd = true;
-				$homeSQL = mysql_query("select locStreet1, locStreet2, locCity, locState, locZIP from resume.res_loc where locID = '".$homeID."'");
+				$homeSQL = mysql_query("select locStreet1, locStreet2, locCity, locState, locZIP from resume_dev.res_loc where locID = '".$homeID."'");
 				while($row = mysql_fetch_array($homeSQL))
 				{
 					$homeS1		= $row['locStreet1'];
@@ -130,7 +130,7 @@ function populateHeader($userID)
 
 			
 			
-			$phoneSQL = mysql_query("select phACode, phNum3, phNum4 from resume.res_phone where phID ='".$phoneID."'");
+			$phoneSQL = mysql_query("select phACode, phNum3, phNum4 from resume_dev.res_phone where phID ='".$phoneID."'");
 			while($row = mysql_fetch_array($phoneSQL))
 			{
 				$phoneNum1 	= $row['phACode'];
@@ -152,24 +152,106 @@ function populateHeader($userID)
 
 function populateEducation($userID)
 {
-	$educationSQL = mysql_query("select edSchoolName, edSchoolCity, edSchoolState, edSchoolCollege, edDegree, edMajor, edMajor2, edStart, edEnd from resume.res_education where userID='".$userID."'");
+	$educationSQL	= mysql_query("select edID, edSchoolName, edSchoolCity, edSchoolState, edCollege, edStart, edEnd from resume_dev.res_education where userID='".$userID."'");
 	while($row = mysql_fetch_array($educationSQL))
 	{
+		$edID	= $row['edID'];
+		
 		echo "<span class='school'>".$row['edSchoolName']."</span>";
-		echo "<span class='timeframe'>".$row['edStart']." &#8211; ".$row['edEnd']."</span>";
+		if ($row['edEnd'] == NULL)
+		{ $edEnd	= "Present"; }
+		else { $edEnd	= $row['edEnd'];}
+		echo "<span class='timeframe'>".$row['edStart']." &#8211; ".$edEnd."</span>";
 		echo "<span class='citystate'>".$row['edSchoolCity'].", ".$row['edSchoolState']."</span><br />";
-		echo "<span class='college'>".$row['edSchoolCollege']."</span><br />";
-		echo "<span class='degree'>".$row['edDegree']."</span><br />";
-		if ($row['edMajor2'] != NULL)
-		{ echo "<span class='major'>Dual Majors:  ".$row['edMajor']." & ".$row['edMajor2']."</span>"; }
-		else
-		{ echo "<span class='major'>Major:  ".$row['edMajor']."</span>"; }
+		echo "<span class='college'>".$row['edCollege']."</span><br />";
+		
+		$degreeSQL	= mysql_query("select edDID, edDegree from resume_dev.res_ed_degree where userID='".$userID."' and edID='".$edID."'");
+		while($line = mysql_fetch_array($degreeSQL))
+		{
+			$edDID	= $line['edDID'];
+			echo "<span class='degree'>".$line['edDegree']."</span><br />";
+			
+			$majorCount	= mysql_query("select count(edMID) as majorCount from resume_dev.res_ed_major where userID='".$userID."' and edID='".$edID."' and edDID='".$edDID."' and majorType='1' group by edDID");
+			while($majors = mysql_fetch_array($majorCount))
+			{ $majorQuant	= $majors['majorCount']; }
+			
+			if ($majorQuant == 3)
+			{
+				echo "<span class='edTitle'>Triple Major:  </span>";
+				majorStuff($userID, $edID, $edDID, $majorQuant);
+			}
+			else if ($majorQuant == 2)
+			{
+				echo "<span class='edTitle'>Double Major:  </span>";
+				majorStuff($userID, $edID, $edDID, $majorQuant);
+			}
+			else 
+			{ 
+				echo "<span class='edTitle'>Major:  </span>";
+				majorStuff($userID, $edID, $edDID, $majorQuant);
+			}
+			
+			$minorCount	= mysql_query("select count(edMID) as minorCount from resume_dev.res_ed_major where userID='".$userID."' and edID='".$edID."' and edDID='".$edDID."' and majorType='2' group by edDID");
+			while($minors = mysql_fetch_array($minorCount))
+			{ $minorQuant	= $minors['minorCount']; }
+			if ($minorQuant > 1)
+			{
+				echo "<span class='edTitle'>Minors:  </span>";
+				minorStuff($userID, $edID, $edDID, $minorQuant);
+			}
+			else if ($minorQuant == 1)
+			{
+				echo "<span class='edTitle'>Minor:  </span>";
+				minorStuff($userID, $edID, $edDID, $minorQuant); 
+			}
+			
+		}
 	}
+}
+
+function majorStuff($userID, $edID, $edDID, $majorQuant)
+{
+	$mCount	= 1;
+	echo "<span class='major'>";
+	
+	$majorSQL	= mysql_query("select edMajor from resume_dev.res_ed_major where userID='".$userID."' and edID='".$edID."' and edDID='".$edDID."' and majorType='1'");
+	while($majors = mysql_fetch_array($majorSQL))
+	{
+		if ($mCount != $majorQuant)
+		{
+			$mCount++;
+			$commaz	= ", ";
+		}
+		else { $commaz	= " ";}
+		echo $majors['edMajor'].$commaz;
+	}
+	
+	echo "</span>";
+}
+
+function minorStuff($userID, $edID, $edDID, $minorQuant)
+{
+	$mCount	= 1;
+	echo "<span class='minor'>";
+	
+	$minorSQL	= mysql_query("select edMajor from resume_dev.res_ed_major where userID='".$userID."' and edID='".$edID."' and edDID='".$edDID."' and majorType='2'");
+	while($minors = mysql_fetch_array($minorSQL))
+	{
+		if ($mCount != $minorQuant)
+		{
+			$mCount++;
+			$commaz	= ", ";
+		}
+		else { $commaz	= " ";}
+		echo $minors['edMajor'].$commaz;
+	}
+	
+	echo "</span>";
 }
 
 function populateRC($userID)
 {
-	$rcSQL = mysql_query("select rcCourseName, rcCourseNum, rcCourseDesc from resume.res_curriculum where userID='".$userID."'");
+	$rcSQL = mysql_query("select rcCourseName, rcCourseNum, rcCourseDesc from resume_dev.res_curriculum where userID='".$userID."'");
 	while($row = mysql_fetch_array($rcSQL))
 	{
 		if ($row[''] != NULL)
@@ -181,7 +263,7 @@ function populateRC($userID)
 
 function populateExp($userID)
 {
-	$expSQL = mysql_query("select peID, peCompName, peCompCity, peCompState, peStart, peEnd, pePosition, pePlug from resume.res_proexp where userID='".$userID."'");
+	$expSQL = mysql_query("select peID, peCompName, peCompCity, peCompState, peStart, peEnd, pePosition, pePlug from resume_dev.res_proexp where userID='".$userID."'");
 	while($row = mysql_fetch_array($expSQL))
 	{
 		echo "<article id='".$row['pePlug']."'>
@@ -195,7 +277,7 @@ function populateExp($userID)
 			<span class='position'>".$row['pePosition']."</span>
 			<br />
 			<ul class='duties'>";
-			$expDetailsSQL = mysql_query("select peDetail from resume.res_pe_details where peID='".$row['peID']."'");
+			$expDetailsSQL = mysql_query("select peDetail from resume_dev.res_pe_details where peID='".$row['peID']."'");
 			while($lines = mysql_fetch_array($expDetailsSQL))
 			{
 				echo "<li>".$lines['peDetail']."</li>";
@@ -207,36 +289,54 @@ function populateExp($userID)
 
 function populateIntAct($userID)
 {
-	$intactSQL = mysql_query("select iaDesc from resume.res_intact where userID='".$userID."'");
+	$intactSQL = mysql_query("select iaDesc from resume_dev.res_intact where userID='".$userID."'");
 	while($row = mysql_fetch_array($intactSQL))
 	{ echo "<li>".$row['iaDesc']."</li>"; }
 }
 
 function populateTechExp($userID)
 {
-	$techexpSQL = mysql_query("select teDesc from resume.res_techexp where userID='".$userID."'");
+	$techexpSQL = mysql_query("select teDesc from resume_dev.res_techexp where userID='".$userID."'");
 	while($row = mysql_fetch_array($techexpSQL))
 	{ echo "<li>".$row['teDesc']."</li>"; }
 }
 
 function populateTechDetails($userID,$teCount)
 {
-	echo "<span class='tlanguages'><em>Languages:\t</em>".populateTEDetails($userID,$teCount)."</span><br />";
-	echo "<span class='tsystems'><em>Operating Systems:\t</em>".populateTEDetails($userID,$teCount)."</span><br />";
-	echo "<span class='tprograms'><em>Programs:\t</em>".populateTEDetails($userID,$teCount)."</span><br />";$techCount++;
-	echo "<span class='tother'><em>Other:\t</em>".populateTEDetails($userID,$teCount)."</span><br />";$techCount++;
+	echo "<br /><span class='teTitle'>Languages:\t\t</span>";populateTEDetails($userID,$teCount);$teCount++;
+	echo "<br /><span class='teTitle'>Operating Systems:\t</span>";populateTEDetails($userID,$teCount);$teCount++;
+	echo "<br /><span class='teTitle'>Programs:\t\t</span>";populateTEDetails($userID,$teCount);$teCount++;
+	echo "<br /><span class='teTitle'>Other:\t\t</span>";populateTEDetails($userID,$teCount);
 }
 
-function populateTEDetails($userID,$teCount)
+function populateTEDetails($userID,$teType)
 {
-	$BIGtechexpSQL = mysql_query("select teDesc, teType from resume_dev.res_techexp where userID='".$userID."' and teType='".$techCount."'");
+	$techCount	= 1;
+	echo "<span class='techDetails'>";
+	$teSQL = mysql_query("select count(teID) as teQuant from resume_dev.res_techexp where userID='".$userID."' and teType='".$teType."' group by teType");
+	while($row = mysql_fetch_array($teSQL))
+	{
+		$teQuant	= $row['teQuant'];
+	}
+	
+	$BIGtechexpSQL = mysql_query("select teDesc from resume_dev.res_techexp where userID='".$userID."' and teType='".$teType."' order by teID");
 	while($row = mysql_fetch_array($BIGtechexpSQL))
-	{ echo $row['teDesc']."; "; }
+	{
+		if ($techCount != $teQuant)
+		{
+			$techCount++;
+			$semicolonz	= "; ";
+		}
+		else { $semicolonz	= "";}
+		echo $row['teDesc'].$semicolonz;
+	}
+	
+	echo "</span>";
 }
 
 function getShortName($userID)
 {
-	$getsn = mysql_query("select userFName, userLName, shortname from resume.res_user where userID='".$userID."'");
+	$getsn = mysql_query("select userFName, userLName, shortname from resume_dev.res_user where userID='".$userID."'");
 	while($row = mysql_fetch_array($getsn))
 	{ 
 		$userFName	= $row['userFName']; 
