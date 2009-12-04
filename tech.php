0e5f6932f17cd6ab21bf6dceda8fbcb416164bfd
Neo Melonas <neo@neomelonas.com>
2009-12-04 02:28:30 -0500

    Fixed a dumb error in the functions

--- tech.php
+++ tech.php
@@ -1,8 +1,8 @@
 <?php
-	if (isset($_GET['user'])
-	{ $userID = $_GET['user']; }
+	if (!isset($_GET['user'])
+	{ $userID = 1; }
 	else
-	{ $userID = '1'; }//Set userID to Neo for now, need to make a "splash" page.//
+	{ $userID = $_GET['user']; }//Set userID to Neo for now, need to make a "splash" page.//
 	include 'lib/php/db.php';
 	include 'lib/php/functions.php';
 ?>
@@ -10,7 +10,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title><? populateName($con, $userID); ?> | R&eacute;sum&eacute;</title>
+		<title><?php populateName($con, $userID); ?> | R&eacute;sum&eacute;</title>
 		<link rel="stylesheet" type="text/css" href="lib/css/reset.css" />
 		<link rel="stylesheet" type="text/css" href="lib/css/res.css" />
 		<!--[if IE]>
@@ -22,7 +22,7 @@
 	<body>
 		<div id="wrapper">
 			<header>
-				<? populateHeader($userID); ?>
+				<?php populateHeader($userID); ?>
 			</header>
 			<nav id="nav">
 				<h4>Click to Expand</h4>
@@ -45,34 +45,34 @@
 				</article>
 				<h2><a href="#" class="te noslip">Technology Experience</a></h2>
 				<article id="tech" class="slip">
-					<? populateTechDetails($userID,$teCount); ?>
+					<?php populateTechDetails($userID,$teCount); ?>
 				</article>
 				<h2><a href="#" class="rc noslip">Relevant Curriculum</a></h2>
 				<article id="curriculum" class="slip">
 					<ul>
-						<? populateRC($userID); ?>
+						<?php populateRC($userID); ?>
 					</ul>
 				</article>
 				<h2><a href="#" class="pe noslip">Professional Experience</a></h2>
 				<article id="experience" class="slip">
-					<? populateExp($userID); ?>
+					<?php populateExp($userID); ?>
 				</article>
 				<h2><a href="#" class="ia noslip">Interests &amp; Activities</a></h2>
 				<article id="intact" class="slip">
 					<ul>
-						<? populateIntAct($userID); ?>
+						<?php populateIntAct($userID); ?>
 					</ul>
 				</article>
 			</div>
 			<footer><div id="footer">
 				<hr />
 				<p>
-					Download As:  <a href="<? getShortName($userID); ?>.pdf" class="noline" title="PDF R&eacute;sum&eacute;">PDF</a> &#8226; <a href="<? getShortName($userID); ?>.docx" class="noline" title="DOCX R&eacute;sum&eacute;">DOCX</a> &#8226; <a href="<? getShortName($userID); ?>.doc" class="noline" title="DOC R&eacute;sum&eacute;">DOC</a> &#8226; <a href="<? getShortName($userID); ?>.zip" class="noline" title="ZIP (PDF & DOCX & DOC R&eacute;sum&eacute;s)">ZIP</a>
+					Download As:  <a href="<?php getShortName($userID); ?>.pdf" class="noline" title="PDF R&eacute;sum&eacute;">PDF</a> &#8226; <a href="<?php getShortName($userID); ?>.docx" class="noline" title="DOCX R&eacute;sum&eacute;">DOCX</a> &#8226; <a href="<?php getShortName($userID); ?>.doc" class="noline" title="DOC R&eacute;sum&eacute;">DOC</a> &#8226; <a href="<?php getShortName($userID); ?>.zip" class="noline" title="ZIP (PDF & DOCX & DOC R&eacute;sum&eacute;s)">ZIP</a>
 				</p>
 				<hr /></div>
 			</footer>
 		</div>
-		<? mysql_close($con); ?>
+		<?php mysql_close($con); ?>
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 		<script type="text/javascript" src="lib/js/slide.min.js"></script>
 	</body>
