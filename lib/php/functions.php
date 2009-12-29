8935f7d0f3717cc8ed1dccef2d6ae8c61b9b3510
Neo Melonas <neo@neomelonas.com>
2009-12-29 13:06:38 -0500

    For goodness sake.

--- lib/php/functions.php
+++ lib/php/functions.php
@@ -39,40 +39,38 @@ function AnotherPageView($userID) {
 	}
 	 echo "<!-- " . $clickCount . " /-->";
 }
-function featured($uriPath) {
+function featured() {
 	$sql = mysql_query("SELECT U.userID, userFName, userLName, DU.clickCount FROM resume_dev2.res_user U INNER JOIN resume_dev2.res_data_user DU on U.userID=DU.userID WHERE featured=1 ORDER BY DU.clickCount DESC LIMIT 5");
 	while($row = mysql_fetch_object($sql)) {
 		$userID = $row->userID;
 		$userName = $row->userFName . " " . $row->userLName;
-		echo "<li><a href='" . $uriPath . "resume/" . $userID . "/'>" . $userName . "</a></li>";
+		echo "<li><a href='/ResumeBeta/resume/" . $userID . "/'>" . $userName . "</a></li>";
 	}
 }
-function recentUpdate($uriPath) {
+function recentUpdate() {
 	$sql = mysql_query("SELECT U.userID, userFName, userLName FROM resume_dev2.res_user U INNER JOIN resume_dev2.res_data_user DU on U.userID=DU.userID ORDER BY DU.lastUpdate DESC LIMIT 5");
 	while($row = mysql_fetch_object($sql)) {
 		$userID = $row->userID;
 		$userName = $row->userFName . " " . $row->userLName;
-		echo "<li><a href='" . $uriPath . "resume/" . $userID . "/'>" . $userName . "</a></li>";
+		echo "<li><a href='/ResumeBeta/resume/" . $userID . "/'>" . $userName . "</a></li>";
 	}
 }
-function recentAddition($uriPath) {
+function recentAddition() {
 	$sql = mysql_query("SELECT U.userID, userFName, userLName FROM resume_dev2.res_user U INNER JOIN resume_dev2.res_data_user DU on U.userID=DU.userID ORDER BY DU.dateCreated DESC LIMIT 5");
 	while($row = mysql_fetch_object($sql)) {
 		$userID = $row->userID;
 		$userName = $row->userFName . " " . $row->userLName;
-		echo "<li><a href='" . $uriPath . "resume/" . $userID . "/'>" . $userName . "</a></li>";
+		echo "<li><a href='/ResumeBeta/resume/" . $userID . "/'>" . $userName . "</a></li>";
 	}
 }
-function mostViewed($uriPath) {
+function mostViewed() {
 	$sql = mysql_query("SELECT U.userID, userFName, userLName, DU.clickCount FROM resume_dev2.res_user U INNER JOIN resume_dev2.res_data_user DU on U.userID=DU.userID ORDER BY DU.clickCount DESC LIMIT 5");
 	while($row = mysql_fetch_object($sql))
 	{
 		$userID = $row->userID;
 		$userName = $row->userFName . " " . $row->userLName;
 		$clicks = $row->clickCount;
-		echo "<li><a href='" . $uriPath . "resume/" . $userID . "/' title='". $clicks ." Views'>" . 
-$userName . "</a>  <span 
-class='canhide'>". $clicks ." Views</span></li>";//" . $uriPath . "
+		echo "<li><a href='/ResumeBeta/resume/" . $userID . "/' title='". $clicks ." Views'>" . $userName . "</a>  <span class='canhide'>". $clicks ." Views</span></li>";//" . $uriPath . "
 	}
 }
 
@@ -81,42 +79,21 @@ class='canhide'>". $clicks ." Views</span></li>";//" . $uriPath . "
 ///////////////////////////
 ///   For The Resumes   ///
 ///////////////////////////
-
-
-function GetUserLocation($userID) {
-	$sql = mysql_query("SELECT L.locID, locStreet, locStreet2, locCity ,locState, locZIP, homeLoc FROM resume_dev2.res_location L INNER JOIN res_user_loc UL ON L.locID=UL.locID WHERE userID='" . $userID . "'");
-	while($row = mysql_fetch_object($sql)) {
-		if (ord($row->homeLoc) == 1)
-		{ $homeLoc = $row->locID; }
-		else
-		{ $localLoc = $row->locID; }
-		
-	}
+function GetUserInfo($userInfo) {
+	$userID = $userInfo['ID'];
+	//$userInfo['name'] = GetUserName($userID);
+	echo "<h1>";GetUserName($userID); echo "</h1>";
+	GetContactInfo($userInfo);
 }
-
-function GetUserEd($userID) {
-	$edSQL = mysql_query("SELECT edID, edStart, edEnd from resume_dev2.res_user_ed where userID='" . $userID . "'");
-}
-function GetUserSpecificEd($userID) {
-	$colSQL = mysql_query("SELECT colID, gradMonth, gradYear from resume_dev2.res_user_col where userID='" . $userID . "'");
-		$degSQL = mysql_query("SELECT degreeID from resume_dev2.res_user_deg where userID='" . $userID . "' and colID='" . $colID . "'");
-			$gpaSQL = mysql_query("SELECT gpa from resume_dev2.res_user_gpa where userID='" . $userID . "' and degreeID = '" . $degreeID . "'");
-		$majSQL = mysql_query("SELECT majorID from resume_dev2.res_user_major where userID='" . $userID . "' and colID='" . $colID . "'");
-}	
-function GetUserMinor($userID) {
-	$minSQL = mysql_query("SELECT minorID from resume_dev2.res_user_minor where userID='" . $userID . "'");
-}
-function GetUserTech($userID) {	
-	$techSQL = mysql_query("SELECT teID from resume_dev2.res_user_tech where userID='" . $userID . "'");
-}
-function GetUserRC($userID) {
-	$rcSQL = mysql_query("SELECT rcID from resume_dev2.res_user_rc where userID='" . $userID . "'");
-}
-function GetUserExpPosition($userID) {
-	$expSQL = mysql_query("SELECT expID, expPosition, expStartMonth, expStartYear, expEndMonth, expEndYear from resume_dev2.res_user_ia where userID='" . $userID . "'");
-}
-function GetUserIA($userID) {
-	$iaSQL = mysql_query("SELECT iaID, iaWeight from resume_dev2.res_user_id where userID='" . $userID . "'");
+function GetContactInfo($userInfo) {
+	GetUserLocation($userInfo);
+	if (isset($userInfo['hAddress']))
+	{ echo $userInfo['hAddress']; }
+	if (isset($userInfo['lAddress']))
+	{ echo $userInfo['lAddress']; }
+	GetUserPhone($userInfo);
+	echo "&bull;";
+	GetUserEmail($userInfo);
 }
 function GetUserName($userID) {
 	$sql = mysql_query("SELECT userFName, userMName, userLName, middleASnick from resume_dev2.res_user where userID=" . $userID . " LIMIT 1");
@@ -134,25 +111,102 @@ function GetUserName($userID) {
 	{ $uName = $ufname . " " . $ulname; }
 	echo $uName;
 }
-function UserLocation($userID) {
-	GetUserLocation($userID);
-	$location = array();
-	$homeSQL = mysql_query("SELECT locStreet, locStreet2, locCity ,locState, locZIP from resume_dev2.res_location where locID='". $homeLoc ."'");
-	if (isset($localLoc)) {
-		$localSQL = mysql_query("SELECT locStreet, locStreet2, locCity ,locState, locZIP from resume_dev2.res_location where locID=". $localLoc ."");
-		while($line = mysql_fetch_object($localSQL)) {
+function GetUserLocation($userInfo) {
+	$userID = $userInfo['ID'];
+	$sql = mysql_query("SELECT L.locID, locStreet, locStreet2, locCity ,locState, locZIP, homeLoc FROM resume_dev2.res_location L INNER JOIN res_user_loc UL ON L.locID=UL.locID WHERE userID='" . $userID . "'");
+	while($row = mysql_fetch_object($sql)) {
+		$homeLoc = null;
+		if (ord($row->homeLoc) == 1)
+		{ $homeLoc = $row->locID; }
+		else
+		{ $localLoc = $row->locID; }
+		$street = $row->locStreet;
+		$city = $row->locCity;
+		$state = $row->locState;
+		$ZIP = $row->locZIP;
+		if (isset($locStreet2)) {
+			$street = $street . " " . $row->locStreet2;
+		}
+		$address = $street . " " . $city . ", " . $state . " " . $ZIP;
+		if (isset($homeLoc)) 
+		{
+			$userInfo['hAddress'] = "<h4><span class='home address'>HOME: " . $address . "</span></h4>";
+			echo $userInfo['hAddress'];
+		}
+		else 
+		{
+			$userInfo['lAddress'] = "<h4><span class='local address'>LOCAL: " . $address . "</span></h4>";
+			echo $userInfo['lAddress'];
 		}
 	}
-	while($row = mysql_fetch_object($homeSQL)) {
-		$location[street] = $row->locStreet;
-		if (isset($row->locStreet2))
-		{ $location[street2] = $row->locStreet2; }
-		$location[city] = $row->locCity;
+}
+function GetUserPhone($userInfo) {
+	$userID = $userInfo['ID'];
+	$sql = mysql_query("SELECT phArea, phZone, phLocal, phType from res_phone where userID='" . $userID . "' and prefPhone='1' LIMIT 1");
+	while($row = mysql_fetch_object($sql)) {
+		$phone = "(" . $row->phArea . ") " . $row->phZone . "-" . $row->phLocal;
+		$userInfo['phone'] = $phone;
+		$userInfo['phType'] = $row->phType;
+		echo "<h4><span class='phone'>" . $userInfo['phone'] . "</span>";
+	}	
+}
+function GetUserEmail($userInfo) {
+	$userID = $userInfo[ID];
+	$sql = mysql_query("SELECT userEmail from res_user where userID='" . $userID . "' LIMIT 1");
+	while($row = mysql_fetch_object($sql)) {
+		$userInfo[email] = $row->userEmail;
+		echo "<span class='email'><a href='mailto:" . $userInfo[email] . "'>" . $userInfo[email] . "</a></span></h4>";
 	}
-	echo $location[city];
-	//$userPrefs['hloc'];
 }
 
+function GetUserEd($userInfo) {
+	$userID = $userInfor[ID];
+	
+	$edSQL = mysql_query("SELECT edID, edName, edCity, edState, edStart, edEnd FROM res_user_ed UE INNER JOIN res_education ED UE.edID=ED.edID WHERE userID='" . $userID . "'");
+	while($row = mysql_fetch_object($edSQL)) {
+		$edID = $row->edID;
+		$edName = $row->edName;
+		$edCity = $row->edCity;
+		$edState = $row->edState;
+		$edStart = $row->edStart;
+		if (!isset($row->edEnd))
+		{ $edEnd = "Present"; }
+		else
+		{ $edEnd = $row->edEnd; }
+		$colSQL = mysql_query("SELECT C.colID, colName, gradMonth, gradYear from resume_dev2.res_user_col UC INNER JOIN resume_dev2.res_ed_col C on UC.colID=C.colID WHERE userID='" . $userID . "' AND C.edID='" . $edID . "'");
+		while($lines = mysql_fetch_object($colSQL)) {
+			$colID = $lines->colID;
+			$colName = $lines->colName;
+			if (isset($lines->gradMonth)) 
+			{ $gradMonth = $lines->gradMonth; }
+			$gradYear = $lines->gradYear;
+			$degSQL = mysql_query("SELECT D.degreeID, degreeName, gpa FROM resume_dev2.res_ed_degree D INNER JOIN  resume_dev2.res_user_deg UD on D.degreeID=UD.degreeID INNER JOIN resume_dev2.res_user_gpa UG on D.degreeID=UG.degreeID WHERE userID='" . $userID . "' and colID='" . $colID . "'");
+			while($paths = mysql_fetch_object($degSQL)) {
+				$degreeID = $paths->degreeID;
+				$degreeName = $paths->degreeName;
+				$gpa = $paths->gpa;
+				echo $gpa;
+			}
+		}
+	}
+
+		$majSQL = mysql_query("SELECT majorID from resume_dev2.res_user_major where userID='" . $userID . "' and colID='" . $colID . "'");
+}	
+function GetUserMinor($userID) {
+	$minSQL = mysql_query("SELECT minorID from resume_dev2.res_user_minor where userID='" . $userID . "'");
+}
+function GetUserTech($userID) {	
+	$techSQL = mysql_query("SELECT teID from resume_dev2.res_user_tech where userID='" . $userID . "'");
+}
+function GetUserRC($userID) {
+	$rcSQL = mysql_query("SELECT rcID from resume_dev2.res_user_rc where userID='" . $userID . "'");
+}
+function GetUserExpPosition($userID) {
+	$expSQL = mysql_query("SELECT expID, expPosition, expStartMonth, expStartYear, expEndMonth, expEndYear from resume_dev2.res_user_ia where userID='" . $userID . "'");
+}
+function GetUserIA($userID) {
+	$iaSQL = mysql_query("SELECT iaID, iaWeight from resume_dev2.res_user_id where userID='" . $userID . "'");
+}
 
 //////////////////////////
 ///   For   Browsing   ///
@@ -169,8 +223,7 @@ function browsing($browse) {
 				$ufname = $row->userFName;
 				$ulname = $row->userLName;
 				$username = $ufname . " " . $ulname;
-				echo "<li><a href=''" . $uriPath . "resume/". $userID . "/'>" . $username 
-."</a></li>";
+				echo "<li><a href='/ResumeBeta/resume/". $userID . "/'>" . $username ."</a></li>";
 			}
 			$letter++;
 			echo "</ul>";
@@ -189,8 +242,7 @@ function browsing($browse) {
 				$ufname = $lines['userFName'];
 				$ulname = $lines['userLName'];
 				$username = $ufname . " " . $ulname;
-				echo "<li><a href='" . $uriPath . "resume/" . $userID . "/>" . $username . 
-"</a></li>";
+				echo "<li><a href='/ResumeBeta/resume/" . $userID . "/>" . $username . "</a></li>";
 			}
 			echo "</ul>";
 		}
@@ -207,8 +259,7 @@ function browsing($browse) {
 				$ufname = $row->userFName;
 				$ulname = $row->userLName;
 				$username = $ufname . " " . $ulname;
-				echo "<li><a href='" . $uriPath . "resume/" . $userID . "'>" . $username . 
-"</a></li>";
+				echo "<li><a href='/ResumeBeta/resume/" . $userID . "'>" . $username . "</a></li>";
 			}
 		}
 	}
@@ -219,8 +270,59 @@ function browsing($browse) {
 ///   For   Admining   ///
 //////////////////////////
 
-function userLogin($userID, $password) {
-	
+function userLogin($userEmail, $password) {
+
 }
 
+//////////////////////////
+///   For	UserInfo   ///
+//////////////////////////
+function FillUserInfo($userInfo) {
+	$userID = $userInfo[ID];
+	
+	$sql = mysql_query("SELECT userFName, userMName, userLName, middleASnick, userEmail from resume_dev2.res_user where userID=" . $userID . " LIMIT 1");
+	while($row = mysql_fetch_object($sql)) {
+		if (isset ($row->userMName))
+		{ $umname = $row->userMName; }
+		if (($row->middleASnick) && (isset($umname)))
+		{ $umname = '"'.$umname.'"'; }
+		$ufname = $row->userFName;
+		$ulname = $row->userLName;
+		$userInfo[email] = $row->userEmail;
+	}
+	if (isset($umname))
+	{ $uName = $ufname . " " .$umname . " " . $ulname;}
+	else
+	{ $uName = $ufname . " " . $ulname; }
+	$userInfo[name] = $uName;
+	
+	$sql = mysql_query("SELECT L.locID, locStreet, locStreet2, locCity ,locState, locZIP, homeLoc FROM resume_dev2.res_location L INNER JOIN res_user_loc UL ON L.locID=UL.locID WHERE userID='" . $userID . "'");
+	while($row = mysql_fetch_object($sql)) {
+		$homeLoc = null;
+		if (ord($row->homeLoc) == 1)
+		{ $homeLoc = $row->locID; }
+		else
+		{ $localLoc = $row->locID; }
+		$street = $row->locStreet;
+		$city = $row->locCity;
+		$state = $row->locState;
+		$ZIP = $row->locZIP;
+		if (isset($locStreet2)) {
+			$street = $street . " " . $row->locStreet2;
+		}
+		$address = $street . " " . $city . ", " . $state . " " . $ZIP;
+		if (isset($homeLoc)) 
+		{ $userInfo['hAddress'] = $address; }
+		else 
+		{ $userInfo['lAddress'] = $address; }
+	}
+	
+	$sql = mysql_query("SELECT phArea, phZone, phLocal, phType from res_phone where userID='" . $userID . "' and prefPhone='1' LIMIT 1");
+	while($row = mysql_fetch_object($sql)) {
+		$phone = "(" . $row->phArea . ") " . $row->phZone . "-" . $row->phLocal;
+		$userInfo['phone'] = $phone;
+		$userInfo['phType'] = $row->phType;
+	}
+	print_r($userInfo);
+}
 ?>
--- res-resume.php
+++ res-resume.php
@@ -4,8 +4,12 @@ if (isset($_GET['u']))
 include ('lib/conf/settings.inc');
 include ('lib/php/functions.php');
 db_connect($con);
-if (isset($userID))
-{ AnotherPageView($userID); }
+if (isset($userID)) {
+	AnotherPageView($userID);
+	$userInfo = array();
+	$userInfo['ID'] = $userID;
+	FillUserInfo($userInfo);
+}
 else //{$userID=1;}
 {die ('SHIIIT'); }
 ?>
@@ -13,35 +17,42 @@ else //{$userID=1;}
 <html>
 	<head>
 		<title><?php GetUserName($userID); ?> | R&eacute;sum&eacute;</title>
-		<link rel="stylesheet" type="text/css" href="<?php echo $uriPath; ?>lib/css/reset.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo $uriPath; ?>lib/css/<?php //echo $restype; ?>res.css" />
-		<!--[if IE]>
-			<link rel="stylesheet" type="text/css" href="lib/css/ie/<?php echo $restype; ?>.css" />
-		<![end if]/-->
+		<link rel="stylesheet" href="/<?php echo $uriPath; ?>lib/css/blueprint/screen.css" type="text/css" media="screen, projection" />
+		<link rel="stylesheet" href="/<?php echo $uriPath; ?>lib/css/blueprint/plugins/fancy-type/screen.css" type="text/css" media="screen, projection" />
+		<link rel="stylesheet" href="/<?php echo $uriPath; ?>lib/css/blueprint/print.css" type="text/css" media="print" />
+		<!--[if lt IE 8]>
+			<link rel="stylesheet" href="/<?php echo $uriPath; ?>lib/css/blueprint/ie.css" type="text/css" media="screen, projection" />
+		<![endif]-->
+		
+		
+		<link rel="stylesheet" type="text/css" href="/<?php echo $uriPath; ?>lib/css/<?php //echo $restype; ?>res.css" />
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<meta name="description" content="Resume" />
-	<head>
+	</head>
 	<body>
-		<div id="wrapper">
-			<header>
+		<div class="container showgrid">
+			<header class="span-24">
 			<div id="header">
-				<h1><?php GetUserName($userID); ?></h1>
-				<h4><?php UserLocation($userID); ?></h4>
+				<?php GetUserInfo($userInfo); ?>
 			</div>
 			</header>
+			<div id="next">
+				<?php GetUserEd($userInfo); ?>
+			</div>
 			<hr />
+			<a href="/ResumeBeta/resetcount.php?u=<?php echo $userInfo['ID']; ?>">REMOVE THIS LINK</a>
 			
 			
-			<?php /*
 			<footer><div id="footer">
 				<hr />
 				<p>
 					Download As:  <a href="<?php //getPath($userID); ?>.pdf" class="noline" title="PDF R&eacute;sum&eacute;">PDF</a> &#8226; <a href="<?php //getShortName($userID); ?>.docx" class="noline" title="DOCX R&eacute;sum&eacute;">DOCX</a> &#8226; <a href="<?php //getShortName($userID); ?>.doc" class="noline" title="DOC R&eacute;sum&eacute;">DOC</a> &#8226; <a href="<?php //getShortName($userID); ?>.zip" class="noline" title="ZIP (PDF &amp; DOCX &amp; DOC R&eacute;sum&eacute;s)">ZIP</a>
 				</p>
 				<hr /></div>
-			</footer> */ ?>
+			</footer> 
 		</div>
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 		<script type="text/javascript" src="lib/js/slide.min.js"></script>
 	</body>
 </html>
+
