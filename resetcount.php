5fd4f211b0b5ebddd53a2483f750e75c0c9de915
Neo Melonas <neo@neomelonas.com>
2009-12-31 16:02:52 -0500

    ???

---
+++ resetcount.php
@@ -0,0 +1,17 @@
+<?php 
+include 'lib/conf/settings.inc';
+$url = $_SERVER['HTTP_REFERER'];
+if (!isset($_GET['u']))
+{
+	header("Location: $url");
+}
+else
+{
+	$userID = $_GET['u'];
+	mysql_select_db($db, $con);
+	mysql_query("update res_data_user set clickCount=0 where 
+userID='" . $userID . "'") or die('NO GO');
+	mysql_close($con);
+	header("Location: $url");
+}
+?>
